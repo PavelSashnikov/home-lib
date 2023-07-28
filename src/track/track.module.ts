92886dcaa3b5c2f6b3d279709bbf5468aa2b2c6a
Pavel Sashnikov <sashnikov24@gmail.com>
2023-07-28 16:22:18 +0300

    feat: add track controller

--- src/track/track.module.ts
+++ src/track/track.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TrackService } from './track.service';
 import { TrackController } from './track.controller';
+import { GlobalModule } from 'src/global/global.module';
 
 @Module({
+  imports: [GlobalModule],
   providers: [TrackService],
-  controllers: [TrackController]
+  controllers: [TrackController],
 })
 export class TrackModule {}
